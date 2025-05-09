a394942ae2f50a87d7829a1e0450bdfdbb2b5d15
suriya2316ss <suriya2311ss@gmail.com>
2025-05-09 11:38:23 +0530

    Delete startup_pitch_platform directory

--- startup_pitch_platform/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from . import consumers
-
-urlpatterns = [
-    path('message/', views.ChatbotAPI.as_view(), name='chatbot_message'),
-    path('sessions/', views.ChatSessionView.as_view(), name='chat_sessions'),
-    path('sessions/<str:session_id>/', views.ChatSessionView.as_view(), name='chat_session_detail'),
-]
--- startup_pitch_platform/startup_pitch_platform/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file marks the directory as a Python package.
-
-# Expose specific modules for easier imports
-# from startup_pitch_platform import views
-# from . import models
-
